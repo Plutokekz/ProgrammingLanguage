04027fb0ee66104b278acc60e1f4f034a420ec5b
BuildTools <unconfigured@null.spigotmc.org>
2021-11-17 18:50:20 +0100

    ADD Parser

---
+++ Parser.py
@@ -0,0 +1,65 @@
+class NodeNumber:
+
+    def __init__(self, token):
+        self.token = token
+
+    def __repr__(self):
+        return f"NodeNumber(token: {self.token})"
+
+
+class NodeBinaryOperation:
+
+    def __init__(self, left_node, operation_token, right_node):
+        self.left_node = left_node
+        self.right_node = right_node
+        self.operation_token = operation_token
+
+    def __repr__(self):
+        return f"NodeBinaryOperation(left: {self.left_node}, operation: {self.operation_token}, right: {self.right_node})"
+
+
+class Parser:
+
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_index = -1
+        self.current_token = None
+        self.advance()
+
+    def advance(self):
+        self.token_index += 1
+        if self.token_index < len(self.tokens):
+            self.current_token = self.tokens[self.token_index]
+        return self.current_token
+
+    def parse(self):
+        return self.expression()
+
+    def factor(self):
+        if self.current_token.token_type in (Token.INTEGER, Token.FLOAT):
+            token = self.current_token
+            self.advance()
+            return NodeNumber(token)
+
+    def term(self):
+        return self._operation(self.factor, (Token.MULTIPLY, Token.DIVISION))
+
+    def _operation(self, func, token_types):
+        left = func()
+        while self.current_token.token_type in token_types:
+            operation_token = self.current_token
+            self.advance()
+            right = func()
+            left = NodeBinaryOperation(left, operation_token, right)
+        return left
+
+    def expression(self):
+        return self._operation(self.term, (Token.ADDITION, Token.SUBTRACTION))
+
+
+if __name__ == "__main__":
+    tokens = [TokenType(Token.INTEGER, "1", None), TokenType(Token.ADDITION, "+", None),
+              TokenType(Token.INTEGER, "2", None), TokenType(Token.MULTIPLY, "*", None),
+              TokenType(Token.INTEGER, "3", None)]
+    parser = Parser(tokens)
+    print(parser.parse())
